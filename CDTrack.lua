9948b24706fb381fcef290180130a10e760a3393
trus <trus8@yandex.ru>
2017-05-28 12:13:42 +0300

    CDtracker now display everything just under HPBar

--- CDTrack.lua
+++ CDTrack.lua
@@ -3,8 +3,8 @@ CAMenu = MenuElement({type = MENU, id = "CAMenu", name = "CD Tracker"})
 CAMenu:MenuElement({id = "Enabled", name = "Enabled", value = true})
 CAMenu:MenuElement({type = MENU, id = "SpellTracker", name = "Spell Tracker"})
 CAMenu.SpellTracker:MenuElement({id = "fontsize", name = "Font size", value = 20, min = 4, max = 40, step = 1, identifier = ""})
-CAMenu.SpellTracker:MenuElement({id = "Xpos", name = "XPos", value = 90, min = -500, max = 500, step = 1, identifier = ""})
-CAMenu.SpellTracker:MenuElement({id = "Ypos", name = "YPos", value = 0, min = -500, max = 500, step = 1, identifier = ""})
+CAMenu.SpellTracker:MenuElement({id = "Xpos", name = "XPos", value = 11, min = -500, max = 500, step = 1, identifier = ""})
+CAMenu.SpellTracker:MenuElement({id = "Ypos", name = "YPos", value = 6, min = -500, max = 500, step = 1, identifier = ""})
 CAMenu.SpellTracker:MenuElement({id = "ShowCD", name = "Show cd numbers", key = string.byte("E")})
 CAMenu.SpellTracker:MenuElement({id = "Enabled", name = "Enabled", value = true})
 CAMenu.SpellTracker:MenuElement({id = "TEnemies", name = "Track Enemies", value = true})
@@ -48,105 +48,105 @@ local function DrawSpellTracking(type, hero)
 		return
 	end
 	
-	if hero.pos2D.onScreen then
+	if hero.hpBar.onScreen then
 		local XposX = CAMenu.SpellTracker.Xpos:Value()
 		local YposY = CAMenu.SpellTracker.Ypos:Value()
 		local fontsize = CAMenu.SpellTracker.fontsize:Value()
 		local shownumbers = CAMenu.SpellTracker.ShowCD:Value()
-		Draw.Rect(hero.pos2D.x-XposX-5-1,hero.pos2D.y+YposY+3+12-1, 118+2 , 12+2 ,Draw.Color(0x7A000000)); --whole bar
+		Draw.Rect(hero.hpBar.x-XposX,hero.hpBar.y+YposY+3+12-1, 118+2 , 12+2 ,Draw.Color(0x7A000000)); --whole bar
 		
-		Draw.Rect(hero.pos2D.x-XposX+3+27*0,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xAA000000)); --spell bars
-		Draw.Rect(hero.pos2D.x-XposX+3+27*1,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xAA000000));
-		Draw.Rect(hero.pos2D.x-XposX+3+27*2,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xAA000000));
-		Draw.Rect(hero.pos2D.x-XposX+3+27*3,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xAA000000));
+		Draw.Rect(hero.hpBar.x-XposX+3+27*0,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xAA000000)); --spell bars
+		Draw.Rect(hero.hpBar.x-XposX+3+27*1,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xAA000000));
+		Draw.Rect(hero.hpBar.x-XposX+3+27*2,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xAA000000));
+		Draw.Rect(hero.hpBar.x-XposX+3+27*3,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xAA000000));
 		
-		Draw.Rect(hero.pos2D.x-XposX-5+121-1,hero.pos2D.y+YposY+3+12-1, 33+2 , 12+2 ,Draw.Color(0x7A00005A)); --whole bar
-		Draw.Rect(hero.pos2D.x-XposX+121,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xAA000000)); --spell bars
+		Draw.Rect(hero.hpBar.x-XposX-5+121-1,hero.hpBar.y+YposY+3+12-1, 33+2 , 12+2 ,Draw.Color(0x7A00005A)); --whole bar
+		Draw.Rect(hero.hpBar.x-XposX+121,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xAA000000)); --spell bars
 		local QData = hero:GetSpellData(_Q);
 		if QData.level > 0 then
 			for z = 1, QData.level do
-				Draw.Rect(hero.pos2D.x-XposX+27*0+z*5-1,hero.pos2D.y+YposY+3+21, 1 , 2 ,Draw.Color(0xFFFFFF00));
+				Draw.Rect(hero.hpBar.x-XposX+27*0+z*5-1,hero.hpBar.y+YposY+3+21, 1 , 2 ,Draw.Color(0xFFFFFF00));
 			end
 			if QData.ammoCurrentCd > 0 then
 				if QData.ammo > 0 then
-					Draw.Rect(hero.pos2D.x-XposX+3+27*0,hero.pos2D.y+YposY+3+16, 23 - ((QData.ammoCurrentCd / QData.ammoCd) * 23) ,4,Draw.Color(0xFFFF7F00));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*0,hero.hpBar.y+YposY+3+16, 23 - ((QData.ammoCurrentCd / QData.ammoCd) * 23) ,4,Draw.Color(0xFFFF7F00));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+3+27*0,hero.pos2D.y+YposY+3+16, 23 - ((QData.ammoCurrentCd / QData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*0,hero.hpBar.y+YposY+3+16, 23 - ((QData.ammoCurrentCd / QData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
 				end
 			else
 				if QData.currentCd > 0 then
 					if shownumbers then 
-						Draw.Text(math.floor(QData.currentCd).."|", fontsize, hero.pos2D.x-XposX+3+27*0, hero.pos2D.y+YposY+3+16, Draw.Color(0xFFFF7F00))
+						Draw.Text(math.floor(QData.currentCd).."|", fontsize, hero.hpBar.x-XposX+3+27*0, hero.hpBar.y+YposY+3+16, Draw.Color(0xFFFF7F00))
 					end
-					Draw.Rect(hero.pos2D.x-XposX+3+27*0,hero.pos2D.y+YposY+3+16, 23 - ((QData.currentCd / QData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*0,hero.hpBar.y+YposY+3+16, 23 - ((QData.currentCd / QData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+3+27*0,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*0,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
 				end;
 			end
 		end
 		local WData = hero:GetSpellData(_W);
 		if WData.level > 0 then
 			for z = 1, WData.level do
-				Draw.Rect(hero.pos2D.x-XposX+27*1+z*5-1,hero.pos2D.y+YposY+3+21, 1 , 2 ,Draw.Color(0xFFFFFF00));
+				Draw.Rect(hero.hpBar.x-XposX+27*1+z*5-1,hero.hpBar.y+YposY+3+21, 1 , 2 ,Draw.Color(0xFFFFFF00));
 			end
 			if WData.ammoCurrentCd > 0 then
 				if WData.ammo > 0 then
-					Draw.Rect(hero.pos2D.x-XposX+3+27*1,hero.pos2D.y+YposY+3+16, 23 - ((WData.ammoCurrentCd / WData.ammoCd) * 23) ,4,Draw.Color(0xFFFF7F00));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*1,hero.hpBar.y+YposY+3+16, 23 - ((WData.ammoCurrentCd / WData.ammoCd) * 23) ,4,Draw.Color(0xFFFF7F00));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+3+27*1,hero.pos2D.y+YposY+3+16, 23 - ((WData.ammoCurrentCd / WData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*1,hero.hpBar.y+YposY+3+16, 23 - ((WData.ammoCurrentCd / WData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
 				end
 			else
 				if WData.currentCd > 0 then
 					if shownumbers then 
-						Draw.Text(math.floor(WData.currentCd).."|", fontsize, hero.pos2D.x-XposX+3+27*1, hero.pos2D.y+YposY+3+16, Draw.Color(0xFFFF7F00))
+						Draw.Text(math.floor(WData.currentCd).."|", fontsize, hero.hpBar.x-XposX+3+27*1, hero.hpBar.y+YposY+3+16, Draw.Color(0xFFFF7F00))
 					end
-					Draw.Rect(hero.pos2D.x-XposX+3+27*1,hero.pos2D.y+YposY+3+16, 23 - ((WData.currentCd / WData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*1,hero.hpBar.y+YposY+3+16, 23 - ((WData.currentCd / WData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+3+27*1,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*1,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
 				end;
 			end;
 		end
 		local EData = hero:GetSpellData(_E);
 		if EData.level > 0 then
 			for z = 1, EData.level do
-				Draw.Rect(hero.pos2D.x-XposX+27*2+z*5-1,hero.pos2D.y+YposY+3+21, 1 , 2 ,Draw.Color(0xFFFFFF00));
+				Draw.Rect(hero.hpBar.x-XposX+27*2+z*5-1,hero.hpBar.y+YposY+3+21, 1 , 2 ,Draw.Color(0xFFFFFF00));
 			end
 			if EData.ammoCurrentCd > 0 then
 				if EData.ammo > 0 then
-					Draw.Rect(hero.pos2D.x-XposX+3+27*2,hero.pos2D.y+YposY+3+16, 23 - ((EData.ammoCurrentCd / EData.ammoCd) * 23) ,4,Draw.Color(0xFFFF7F00));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*2,hero.hpBar.y+YposY+3+16, 23 - ((EData.ammoCurrentCd / EData.ammoCd) * 23) ,4,Draw.Color(0xFFFF7F00));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+3+27*2,hero.pos2D.y+YposY+3+16, 23 - ((EData.ammoCurrentCd / EData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*2,hero.hpBar.y+YposY+3+16, 23 - ((EData.ammoCurrentCd / EData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
 				end
 			else
 				if EData.currentCd > 0 then
 					if shownumbers then 
-						Draw.Text(math.floor(EData.currentCd).."|", fontsize, hero.pos2D.x-XposX+3+27*2, hero.pos2D.y+YposY+3+16, Draw.Color(0xFFFF7F00))
+						Draw.Text(math.floor(EData.currentCd).."|", fontsize, hero.hpBar.x-XposX+3+27*2, hero.hpBar.y+YposY+3+16, Draw.Color(0xFFFF7F00))
 					end
-					Draw.Rect(hero.pos2D.x-XposX+3+27*2,hero.pos2D.y+YposY+3+16, 23 - ((EData.currentCd / EData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*2,hero.hpBar.y+YposY+3+16, 23 - ((EData.currentCd / EData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+3+27*2,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*2,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
 				end;
 			end;
 		end
 		local RData = hero:GetSpellData(_R);
 		if RData.level > 0 then
 			for z = 1, RData.level do
-				Draw.Rect(hero.pos2D.x-XposX+27*3+z*5-1,hero.pos2D.y+YposY+3+21, 1 , 2 ,Draw.Color(0xFFFFFF00));
+				Draw.Rect(hero.hpBar.x-XposX+27*3+z*5-1,hero.hpBar.y+YposY+3+21, 1 , 2 ,Draw.Color(0xFFFFFF00));
 			end
 			if RData.ammoCurrentCd > 0 then
 				if RData.ammo > 0 then
-					Draw.Rect(hero.pos2D.x-XposX+3+27*3,hero.pos2D.y+YposY+3+16, 23 - ((RData.ammoCurrentCd / RData.ammoCd) * 23) ,4,Draw.Color(0xFFFF7F00));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*3,hero.hpBar.y+YposY+3+16, 23 - ((RData.ammoCurrentCd / RData.ammoCd) * 23) ,4,Draw.Color(0xFFFF7F00));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+3+27*3,hero.pos2D.y+YposY+3+16, 23 - ((RData.ammoCurrentCd / RData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*3,hero.hpBar.y+YposY+3+16, 23 - ((RData.ammoCurrentCd / RData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
 				end
 			else
 				if RData.currentCd > 0 then
 					if shownumbers then 
-						Draw.Text(math.floor(RData.currentCd).."|", fontsize, hero.pos2D.x-XposX+3+27*3, hero.pos2D.y+YposY+3+16, Draw.Color(0xFFFF7F00))
+						Draw.Text(math.floor(RData.currentCd).."|", fontsize, hero.hpBar.x-XposX+3+27*3, hero.hpBar.y+YposY+3+16, Draw.Color(0xFFFF7F00))
 					end
-					Draw.Rect(hero.pos2D.x-XposX+3+27*3,hero.pos2D.y+YposY+3+16, 23 - ((RData.currentCd / RData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*3,hero.hpBar.y+YposY+3+16, 23 - ((RData.currentCd / RData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+3+27*3,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
+					Draw.Rect(hero.hpBar.x-XposX+3+27*3,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
 				end;
 			end;
 		end
@@ -154,18 +154,18 @@ local function DrawSpellTracking(type, hero)
 		if TData.level > 0 then
 			if TData.ammoCurrentCd > 0 then
 				if TData.ammo > 0 then
-					Draw.Rect(hero.pos2D.x-XposX+121,hero.pos2D.y+YposY+3+16, 23 - ((TData.ammoCurrentCd / TData.ammoCd) * 23) ,4,Draw.Color(0xFFFFAD00));
+					Draw.Rect(hero.hpBar.x-XposX+121,hero.hpBar.y+YposY+3+16, 23 - ((TData.ammoCurrentCd / TData.ammoCd) * 23) ,4,Draw.Color(0xFFFFAD00));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+121,hero.pos2D.y+YposY+3+16, 23 - ((TData.ammoCurrentCd / TData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+121,hero.hpBar.y+YposY+3+16, 23 - ((TData.ammoCurrentCd / TData.ammoCd) * 23) ,4,Draw.Color(0xFFFF0000));
 				end
 			else
 				if TData.currentCd > 0 then
 					if shownumbers then 
-						Draw.Text(math.floor(TData.currentCd).."|", fontsize, hero.pos2D.x-XposX+121, hero.pos2D.y+YposY+3+16, Draw.Color(0xFFFF7F00))
+						Draw.Text(math.floor(TData.currentCd).."|", fontsize, hero.hpBar.x-XposX+121, hero.hpBar.y+YposY+3+16, Draw.Color(0xFFFF7F00))
 					end
-					Draw.Rect(hero.pos2D.x-XposX+121,hero.pos2D.y+YposY+3+16, 23 - ((TData.currentCd / TData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+121,hero.hpBar.y+YposY+3+16, 23 - ((TData.currentCd / TData.cd) * 23) ,4,Draw.Color(0xFFFF0000));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+121,hero.pos2D.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
+					Draw.Rect(hero.hpBar.x-XposX+121,hero.hpBar.y+YposY+3+16,23,4,Draw.Color(0xFF00FF00));
 				end;
 			end;
 		end
@@ -174,27 +174,27 @@ local function DrawSpellTracking(type, hero)
 			local SpellYOffset = 0;
 			if DData.ammoCurrentCd > 0 then
 				if DData.ammo > 0 then
-					Draw.Rect(hero.pos2D.x-XposX+120+33-1, hero.pos2D.y+YposY+3+12-1,14,14,Draw.Color(0xFFFFAD00));
+					Draw.Rect(hero.hpBar.x-XposX+120+33-1, hero.hpBar.y+YposY+3+12-1,14,14,Draw.Color(0xFFFFAD00));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+120+33-1, hero.pos2D.y+YposY+3+12-1,14,14,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+120+33-1, hero.hpBar.y+YposY+3+12-1,14,14,Draw.Color(0xFFFF0000));
 				end
 				SpellYOffset = math.max( (228 - math.ceil((DData.ammoCurrentCd / DData.ammoCd) * 20) * 12) ,0);
 			else
 				if DData.currentCd > 0 then
 					if shownumbers then 
-						Draw.Text(math.floor(DData.currentCd).."|", fontsize, hero.pos2D.x-XposX+120+33+35, hero.pos2D.y+YposY+3, Draw.Color(0xFFFF7F00))
+						Draw.Text(math.floor(DData.currentCd).."|", fontsize, hero.hpBar.x-XposX+120+33+35, hero.hpBar.y+YposY+3, Draw.Color(0xFFFF7F00))
 					end
-					Draw.Rect(hero.pos2D.x-XposX+120+33-1, hero.pos2D.y+YposY+3+12-1,14,14,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+120+33-1, hero.hpBar.y+YposY+3+12-1,14,14,Draw.Color(0xFFFF0000));
 					SpellYOffset = math.max( (228 - math.ceil((DData.currentCd / DData.cd) * 20) * 12) ,0);
 				else
-					Draw.Rect(hero.pos2D.x-XposX+120+33-1, hero.pos2D.y+YposY+3+12-1,14,14,Draw.Color(0xFF00FF00));
+					Draw.Rect(hero.hpBar.x-XposX+120+33-1, hero.hpBar.y+YposY+3+12-1,14,14,Draw.Color(0xFF00FF00));
 					SpellYOffset = 228;
 				end;
 			end;
 			local SprIdx1 = GetSpriteByName(DData.name);
 			if SprIdx1 ~= nil then
 				local sprCut = {x = 0, y = SpellYOffset, w = 12, h = SpellYOffset+12}
-				SprIdx1:Draw(sprCut, hero.pos2D.x-XposX+120+33, hero.pos2D.y+YposY+3+12);
+				SprIdx1:Draw(sprCut, hero.hpBar.x-XposX+120+33, hero.hpBar.y+YposY+3+12);
 			end
 		end
 		local FData = hero:GetSpellData(SUMMONER_2);
@@ -202,28 +202,28 @@ local function DrawSpellTracking(type, hero)
 			local SpellYOffset = 0;
 			if FData.ammoCurrentCd > 0 then
 				if FData.ammo > 0 then
-					Draw.Rect(hero.pos2D.x-XposX+120+33-1+16, hero.pos2D.y+YposY+3+12-1,14,14,Draw.Color(0xFFFFAD00));
+					Draw.Rect(hero.hpBar.x-XposX+120+33-1+16, hero.hpBar.y+YposY+3+12-1,14,14,Draw.Color(0xFFFFAD00));
 				else
-					Draw.Rect(hero.pos2D.x-XposX+120+33-1+16, hero.pos2D.y+YposY+3+12-1,14,14,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+120+33-1+16, hero.hpBar.y+YposY+3+12-1,14,14,Draw.Color(0xFFFF0000));
 				end
 				SpellYOffset = math.max( (228 - math.ceil((FData.ammoCurrentCd / FData.ammoCd) * 20) * 12) ,0);
 			else
 				if FData.currentCd > 0 then
 					if shownumbers then 
-						Draw.Text(math.floor(FData.currentCd).."|", fontsize, hero.pos2D.x-XposX+120+33+75, hero.pos2D.y+YposY+3, Draw.Color(0xFFFF7F00))
+						Draw.Text(math.floor(FData.currentCd).."|", fontsize, hero.hpBar.x-XposX+120+33+75, hero.hpBar.y+YposY+3, Draw.Color(0xFFFF7F00))
 					end
-					Draw.Rect(hero.pos2D.x-XposX+120+33-1+16, hero.pos2D.y+YposY+3+12-1,14,14,Draw.Color(0xFFFF0000));
+					Draw.Rect(hero.hpBar.x-XposX+120+33-1+16, hero.hpBar.y+YposY+3+12-1,14,14,Draw.Color(0xFFFF0000));
 					SpellYOffset = math.max( (228 - math.ceil((FData.currentCd / FData.cd) * 20) * 12) ,0);
 				else
-					Draw.Rect(hero.pos2D.x-XposX+120+33-1+16, hero.pos2D.y+YposY+3+12-1,14,14,Draw.Color(0xFF00FF00));
+					Draw.Rect(hero.hpBar.x-XposX+120+33-1+16, hero.hpBar.y+YposY+3+12-1,14,14,Draw.Color(0xFF00FF00));
 					SpellYOffset = 228;
 				end;
 			end;
 			local SprIdx2 = GetSpriteByName(FData.name);
 			if SprIdx2 ~= nil then
 				local sprCut = {x = 0, y = SpellYOffset, w = 12, h = SpellYOffset+12}
-				--DrawSprite(SprIdx2, hero.pos2D.x-XposX+120+33+16, hero.pos2D.y+YposY+3+12, 0, SpellYOffset, 12, SpellYOffset+12, 0xffFFFFFF);
-				SprIdx2:Draw(sprCut, hero.pos2D.x-XposX+120+33+16, hero.pos2D.y+YposY+3+12);
+				--DrawSprite(SprIdx2, hero.hpBar.x-XposX+120+33+16, hero.hpBar.y+YposY+3+12, 0, SpellYOffset, 12, SpellYOffset+12, 0xffFFFFFF);
+				SprIdx2:Draw(sprCut, hero.hpBar.x-XposX+120+33+16, hero.hpBar.y+YposY+3+12);
 			end
 		end
 	end
@@ -236,7 +236,7 @@ function TOnDraw()
 			local hero = Game.Hero(i)
 			if hero.alive and hero.visible then
 				if CAMenu.SpellTracker.Enabled:Value() then
-					if hero.pos2D.onScreen then
+					if hero.hpBar.onScreen then
 						DrawSpellTracking(hero.isEnemy and "TEnemies" or hero.isAlly and "TAllies" or "noidea", hero);
 					end
 				end
