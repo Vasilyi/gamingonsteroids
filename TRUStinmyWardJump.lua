f751ef800e9f849c04d7f3ea3af4cb02dfbcfe60
trus <trus8@yandex.ru>
2017-08-29 17:14:35 +0300

    TRUSt in my WardJumps update

--- TRUStinmyWardJump.lua
+++ TRUStinmyWardJump.lua
@@ -1,14 +1,15 @@
---[v1.0]]
-local Scriptname,Version,Author,LVersion = "TRUSt in my WardJump","v1.0","TRUS","7.4"
+local Scriptname,Version,Author,LVersion = "TRUSt in my WardJump","v1.1","TRUS","7.17"
 
 class "WardJump"
-keybindings = { [ITEM_1] = HK_ITEM_1, [ITEM_2] = HK_ITEM_2, [ITEM_3] = HK_ITEM_3, [ITEM_4] = HK_ITEM_4, [ITEM_5] = HK_ITEM_5, [ITEM_6] = HK_ITEM_6,[ITEM_7] = HK_ITEM_7, [_W] = HK_W, [_R] = HK_R }
+keybindings = { [ITEM_1] = HK_ITEM_1, [ITEM_2] = HK_ITEM_2, [ITEM_3] = HK_ITEM_3, [ITEM_4] = HK_ITEM_4, [ITEM_5] = HK_ITEM_5, [ITEM_6] = HK_ITEM_6,[ITEM_7] = HK_ITEM_7, [_Q] = HK_Q, [_W] = HK_W, [_E] = HK_E, [_R] = HK_R }
 local _wards = {2045, 2049, 2050, 2301, 2302, 2303, 3340, 3361, 3362, 3711, 1408, 1409, 1410, 1411, 2043, 2055}
 function WardJump:__init()
 	if (myHero.charName == "LeeSin") then
 		JumpSlot = _W
 	elseif (myHero.charName == "Jax") then
 		JumpSlot = _Q
+	elseif (myHero.charName == "Katarina") then
+		JumpSlot = _E
 	end
 	if (JumpSlot == nil) then
 		return
@@ -46,17 +47,44 @@ end
 
 function WardJump:findWardNearestMouse()
 	local closestDistance, closest = math.huge, nil
-	for i = 1, Game.ObjectCount() do
-		local object = Game.Object(i)
+	for i = 1, Game.WardCount() do
+		local object = Game.Ward(i)
 		if object~=nil then
-			if object.team ~= TEAM_ENEMY then
-				if (myHero.charName ~= "LeeSin" or object.isAlly) then
-					if not self:IsWrongTarget(object) and myHero.pos:DistanceTo(object.pos) < 625 then
-						local currentDistance = object.pos:DistanceTo(mousePos)
-						if currentDistance < closestDistance then
-							closestDistance = currentDistance
-							closest = object
-						end
+			if (myHero.charName ~= "LeeSin" or object.isAlly) then
+				if not self:IsWrongTarget(object) and myHero.pos:DistanceTo(object.pos) < 625 then
+					local currentDistance = object.pos:DistanceTo(mousePos)
+					if currentDistance < closestDistance then
+						closestDistance = currentDistance
+						closest = object
+					end
+				end
+			end
+		end
+	end
+	for i = 1, Game.MinionCount() do
+		local object = Game.Minion(i)
+		if object~=nil then
+			if (myHero.charName ~= "LeeSin" or object.isAlly) then
+				if not self:IsWrongTarget(object) and myHero.pos:DistanceTo(object.pos) < 625 then
+					local currentDistance = object.pos:DistanceTo(mousePos)
+					if currentDistance < closestDistance then
+						closestDistance = currentDistance
+						closest = object
+					end
+				end
+			end
+		end
+	end
+	
+	for i = 1, Game.HeroCount() do
+		local object = Game.Hero(i)
+		if object~=nil then
+			if (myHero.charName ~= "LeeSin" or object.isAlly) then
+				if not self:IsWrongTarget(object) and myHero.pos:DistanceTo(object.pos) < 625 then
+					local currentDistance = object.pos:DistanceTo(mousePos)
+					if currentDistance < closestDistance then
+						closestDistance = currentDistance
+						closest = object
 					end
 				end
 			end
@@ -65,37 +93,15 @@ function WardJump:findWardNearestMouse()
 	return closest, closestDistance
 end
 
-
-local delayedActions, delayedActionsExecuter = {}, nil
-function DelayAction(func, delay, args) --delay in seconds
-	if not delayedActionsExecuter then
-		function delayedActionsExecuter()
-			for t, funcs in pairs(delayedActions) do
-				if t <= os.clock() then
-					for _, f in ipairs(funcs) do f.func(table.unpack(f.args or {})) end
-					delayedActions[t] = nil
-				end
-			end
-		end
-		Callback.Add("Tick", function() delayedActionsExecuter() end)
-	end
-	local t = os.clock() + (delay or 0)
-	if delayedActions[t] then table.insert(delayedActions[t], { func = func, args = args })
-	else delayedActions[t] = { { func = func, args = args } }
-	end
+function WardJump:Cast(spell,pos)
+	Control.SetCursorPos(pos)
+	Control.KeyDown(spell)
+	Control.KeyUp(spell)
 end
 
-
-function jump(pos)
-	if myHero.charName ~= "LeeSin" or myHero:GetSpellData(_W).name == "BlindMonkWOne" then
-		Control.CastSpell(keybindings[JumpSlot], pos);
-	end
-end
 
 local lastward = 0
 function WardJump:OnWndMsg(key, param)
-	
-	
 	local mouseRadius = self.Menu.mouseradius:Value()
 	if self.Menu.JumpKey:Value() and self:CanCast(JumpSlot) and (myHero.charName ~= "LeeSin" or myHero:GetSpellData(_W).name == "BlindMonkWOne") then
 		local wardslot = nil
@@ -108,19 +114,17 @@ function WardJump:OnWndMsg(key, param)
 			local ward,dis = self:findWardNearestMouse()
 			if ward~=nil and dis~=nil and dis<mouseRadius then
 				if myHero.pos:DistanceTo(ward.pos) <=625 then
-					Control.CastSpell(keybindings[JumpSlot], ward.pos);
+					self:Cast(keybindings[JumpSlot], ward.pos);
 				end
 			elseif GetTickCount() > lastward + 200 then
 				lastward = GetTickCount()
 				if myHero.pos:DistanceTo(mousePos) < 600 then
-					Control.CastSpell(keybindings[wardslot], mousePos)
-					DelayAction(jump, 0.1, { mousePos })
-					DelayAction(jump, 0.2, { mousePos })
+					self:Cast(keybindings[wardslot], mousePos)
+					self:Cast(keybindings[JumpSlot], mousePos)
 				else
 					newpos = myHero.pos:Extended(mousePos,600)
-					Control.CastSpell(keybindings[wardslot], newpos)
-					DelayAction(jump, 0.1, { newpos })
-					DelayAction(jump, 0.2, { newpos })
+					self:Cast(keybindings[wardslot], newpos)
+					self:Cast(keybindings[JumpSlot], newpos)
 				end
 			end
 		end
